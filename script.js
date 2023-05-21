1d08bae7dc9ba9cba16a8955ead3ab0da99b15bd
zaki tayyip <zakidayib1997@gmail.com>
2023-05-21 12:25:06 +0300

    tipCalc-2 solved

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("hello");
+
+
+
+let tipCalc = (bill)=>{
+    let tip, total;
+    
+    if(bill >= 50 && bill <= 300){
+         tip = bill*(15/100);
+         total = bill + tip;
+    }
+
+    else {
+        tip = bill*(20/100);
+        total = tip + bill;
+    }
+
+    return `The bill was ${bill}, the tip was ${tip}, and the total value ${total}`;
+}
+
+
+console.log(tipCalc(275));
